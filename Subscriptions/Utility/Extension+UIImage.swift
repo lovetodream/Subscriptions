90c0cfe8d25f5246ca31aa1b80f41014b99c9018
Timo <38291523+lovetodream@users.noreply.github.com>
2022-01-22 20:44:39 +0100

    refactor: move UIImage extension to its own file
    This addresses #7

---
+++ Subscriptions/Utility/Extension+UIImage.swift
@@ -0,0 +1,30 @@
+// 
+//  Extension+UIImage.swift
+//  Subscriptions
+// 
+//  Created by Timo Zacherl on 22.01.22.
+// 
+//  Copyright © 2022 Timo Zacherl. All rights reserved.
+// 
+//  This program is licensed under the GPL-3.0 License.
+//  A copy of that license should be attached to the bundle.
+//  If not, see <https://www.gnu.org/licenses/>
+// 
+
+import UIKit.UIImage
+
+extension UIImage {
+    var averageColor: UIColor? {
+        guard let inputImage = CIImage(image: self) else { return nil }
+        let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+
+        guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
+        guard let outputImage = filter.outputImage else { return nil }
+
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: [.workingColorSpace: kCFNull as Any])
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+
+        return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
+    }
+}
