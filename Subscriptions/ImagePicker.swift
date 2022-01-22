7efbdb73fad3eafc2defd2db568c9648155f8507
Timo <38291523+lovetodream@users.noreply.github.com>
2022-01-22 11:48:53 +0100

    docs: add file headers

--- Subscriptions/ImagePicker.swift
+++ Subscriptions/ImagePicker.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Timo Zacherl on 09.01.22.
 //
+//  Copyright © 2022 Timo Zacherl. All rights reserved.
+//
+//  This program is licensed under the GPL-3.0 License.
+//  A copy of that license should be attached to the bundle.
+//  If not, see <https://www.gnu.org/licenses/>
+//
 
 import UIKit
 import SwiftUI
--- Subscriptions/SubscriptionsApp.swift
+++ Subscriptions/SubscriptionsApp.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Timo Zacherl on 14.12.21.
 //
+//  Copyright © 2021 Timo Zacherl. All rights reserved.
+//
+//  This program is licensed under the GPL-3.0 License.
+//  A copy of that license should be attached to the bundle.
+//  If not, see <https://www.gnu.org/licenses/>
+//
 
 import SwiftUI
 
