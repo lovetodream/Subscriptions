7ea35284977f2771c6df521b9e83de901de7c595
Timo <38291523+lovetodream@users.noreply.github.com>
2022-02-03 16:45:57 +0100

    refactor: reduce code duplication

--- Subscriptions/Utility/DateCalculations.swift
+++ Subscriptions/Utility/DateCalculations.swift
@@ -35,73 +35,15 @@ extension Date {
 }
 
 func calculateCostForRestOfCurrentMonth(for items: FetchedResults<Item>, fullMonth: Bool = false) -> Decimal {
-    let today = fullMonth ? .now.startOfMonth().addingTimeInterval(TimeInterval(60 * 60 * 24)) : Calendar.current.startOfDay(for: .now)
-    let startOfCurrentMonth = today.startOfMonth()
-    let endOfCurrentMonth = today.endOfMonth()
-    let remainingDays = Calendar.current.numberOfDaysBetween(today, and: endOfCurrentMonth) + (fullMonth ? 1 : 0)
-    var fullCost: Decimal = 0
-    for item in items {
-        let billing = BillingOption(item.billing)
-        if let firstBilling = item.lastBillDate, let cost = item.cost?.decimalValue, cost > Decimal(0.0) {
-            switch billing {
-            case .daily:
-                fullCost += Decimal(remainingDays) * cost
-            case .weekly:
-                let weekdayOfFirstBill = Calendar.current.dateComponents([.weekday], from: firstBilling).weekday!
-                var nextBilling = Calendar.current.date(bySetting: .weekday, value: weekdayOfFirstBill, of: today)!
-                if nextBilling.compare(today) == .orderedAscending {
-                    nextBilling = Calendar.current.date(byAdding: .weekOfYear, value: 1, to: nextBilling)!
-                }
-                let occurrences = Calendar.current.dateComponents([.weekday], from: nextBilling, to: today.endOfMonth()).weekday! / 7
-                fullCost += Decimal(occurrences) * cost
-            case .monthly:
-                var components = DateComponents()
-                components.day = Calendar.current.dateComponents([.day], from: firstBilling).day!
-                let currentMonthBilling = Calendar.current.date(bySetting: .day, value: components.day!, of: firstBilling)!
-                if currentMonthBilling.compare(today) != .orderedAscending && currentMonthBilling.compare(endOfCurrentMonth) != .orderedDescending {
-                    fullCost += cost
-                }
-            case .quarterly:
-                var currentDateAtLoop = firstBilling
-                var components = DateComponents()
-                components.month = 3
-                while currentDateAtLoop.compare(endOfCurrentMonth) != .orderedDescending {
-                    if currentDateAtLoop.compare(today) != .orderedAscending {
-                        fullCost += cost
-                    }
-                    currentDateAtLoop = Calendar.current.date(byAdding: components, to: currentDateAtLoop)!
-                }
-            case .everySixMonths:
-                var currentDateAtLoop = firstBilling
-                var components = DateComponents()
-                components.month = 6
-                while currentDateAtLoop.compare(endOfCurrentMonth) != .orderedDescending {
-                    if currentDateAtLoop.compare(today) != .orderedAscending {
-                        fullCost += cost
-                    }
-                    currentDateAtLoop = Calendar.current.date(byAdding: components, to: currentDateAtLoop)!
-                }
-            case .annually:
-                var components = Calendar.current.dateComponents([.year, .month, .day], from: firstBilling)
-                components.year = Calendar.current.component(.year, from: today)
-                let currentYearBilling = Calendar.current.date(from: components)!
-                if currentYearBilling.compare(today) != .orderedAscending && currentYearBilling.startOfMonth() == startOfCurrentMonth {
-                    fullCost += cost
-                }
-            case .custom:
-                let roundedBilling = Int(item.billing.rounded(.down))
-                var currentDateAtLoop = firstBilling
-                while currentDateAtLoop.compare(endOfCurrentMonth) != .orderedDescending {
-                    if currentDateAtLoop.compare(today) != .orderedAscending {
-                        fullCost += cost
-                    }
-                    currentDateAtLoop = Calendar.current.date(byAdding: .day, value: roundedBilling, to: currentDateAtLoop)!
-                }
-            }
+    var cost: Decimal = 0.0
+
+    items.forEach { item in
+        if inCurrentMonth(item, circle: BillingOption(item.billing), fullMonth: fullMonth) {
+            cost += item.cost?.decimalValue ?? 0.0
         }
     }
-    
-    return fullCost
+
+    return cost
 }
 
 func getPreviousBill(_ item: Item, circle: BillingOption) -> Date? {
@@ -228,63 +170,18 @@ func getNextBill(_ item: Item, circle: BillingOption) -> Date? {
 }
 
 func inCurrentMonth(_ item: Item, circle: BillingOption, fullMonth: Bool = false) -> Bool {
-    let today = fullMonth ? .now.startOfMonth().addingTimeInterval(TimeInterval(60 * 60 * 24)) : Calendar.current.startOfDay(for: .now)
-    let startOfCurrentMonth = today.startOfMonth()
-    let endOfCurrentMonth = today.endOfMonth()
-    let remainingDays = Calendar.current.numberOfDaysBetween(today, and: endOfCurrentMonth) + (fullMonth ? 1 : 0)
-    if let firstBilling = item.lastBillDate, let cost = item.cost?.decimalValue, cost > Decimal(0.0) {
-        switch circle {
-        case .daily:
-            return remainingDays > 0
-        case .weekly:
-            let weekdayOfFirstBill = Calendar.current.dateComponents([.weekday], from: firstBilling).weekday!
-            var nextBilling = Calendar.current.date(bySetting: .weekday, value: weekdayOfFirstBill, of: today)!
-            if nextBilling.compare(today) == .orderedAscending {
-                nextBilling = Calendar.current.date(byAdding: .weekOfYear, value: 1, to: nextBilling)!
-            }
-            let occurrences = Calendar.current.dateComponents([.weekday], from: nextBilling, to: today.endOfMonth()).weekday! / 7
-            return occurrences > 0
-        case .monthly:
-            var components = DateComponents()
-            components.day = Calendar.current.dateComponents([.day], from: firstBilling).day!
-            let currentMonthBilling = Calendar.current.date(bySetting: .day, value: components.day!, of: firstBilling)!
-            if currentMonthBilling.compare(today) != .orderedAscending && currentMonthBilling.compare(endOfCurrentMonth) != .orderedDescending {
-                return true
-            }
-        case .quarterly:
-            let components = Calendar.current.dateComponents([.year, .quarter], from: today)
-            var nextBilling = Calendar.current.date(bySetting: .year, value: components.year!, of: firstBilling)!
-            nextBilling = Calendar.current.date(bySetting: .quarter, value: components.quarter!, of: nextBilling)!
-            if nextBilling.compare(today) != .orderedAscending && nextBilling.startOfMonth() == startOfCurrentMonth {
-                return true
-            }
-        case .everySixMonths:
-            let components = Calendar.current.dateComponents([.year, .weekOfYear], from: today)
-            var nextBillingOpt1 = Calendar.current.date(bySetting: .year, value: components.year!, of: firstBilling)!
-            nextBillingOpt1 = Calendar.current.date(bySetting: .weekOfYear, value: components.weekOfYear!, of: nextBillingOpt1)!
-            let nextBillingOpt2 = Calendar.current.date(byAdding: .weekOfYear, value: 52 / 2, to: nextBillingOpt1)!
-            let nextBillingOpt3 = Calendar.current.date(byAdding: .weekOfYear, value: 52 / 2 * -1, to: nextBillingOpt1)!
-            if (nextBillingOpt1.compare(today) != .orderedAscending && nextBillingOpt1.startOfMonth() == startOfCurrentMonth) ||
-                (nextBillingOpt2.compare(today) != .orderedAscending && nextBillingOpt2.startOfMonth() == startOfCurrentMonth) ||
-                (nextBillingOpt3.compare(today) != .orderedAscending && nextBillingOpt3.startOfMonth() == startOfCurrentMonth) {
-                return true
-            }
-        case .annually:
-            let components = Calendar.current.dateComponents([.year], from: today)
-            let currentYearBilling = Calendar.current.date(bySetting: .year, value: components.year!, of: firstBilling)!
-            if currentYearBilling.compare(today) != .orderedAscending && currentYearBilling.startOfMonth() == startOfCurrentMonth {
-                return true
-            }
-        case .custom:
-            let roundedBilling = Int(item.billing.rounded(.down))
-            var currentDateAtLoop = firstBilling
-            while currentDateAtLoop.compare(endOfCurrentMonth) != .orderedDescending {
-                if currentDateAtLoop.compare(today) != .orderedAscending {
-                    return true
-                }
-                currentDateAtLoop = Calendar.current.date(byAdding: .day, value: roundedBilling, to: currentDateAtLoop)!
-            }
-        }
+    let previousBill = getPreviousBill(item, circle: circle)
+    let nextBill = getNextBill(item, circle: circle)
+    let today = Calendar.current.startOfDay(for: .now)
+    let firstDay = fullMonth ? today.startOfMonth() : today
+    let endOfMonth = today.endOfMonth()
+    
+    if let nextBill = nextBill, nextBill >= firstDay, nextBill <= endOfMonth {
+        return true
+    }
+    
+    if let previousBill = previousBill, fullMonth, previousBill >= firstDay, previousBill <= endOfMonth {
+        return true
     }
     
     return false
